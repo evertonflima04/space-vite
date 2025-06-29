eb73229cb48a4c4bc915b715138af6ae8f2e752a
Everton Ferreira Lima <Evertonlima0406@hotmail.com>
2025-06-29 17:39:49 -0300

    Arrumando o espaçamento e o banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { styled } from "styled-components";
 import EstilosGlobais from "./componetes/EstilosGlobais";
 import Cabecalho from "./componetes/Cabecalho";
 import BarraLateral from "./componetes/BarraLateral";
+import Banner from "./componetes/Banner";
+import bannerBackgroud from "./assets/banner.png";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(
@@ -14,14 +16,33 @@ const FundoGradiente = styled.div`
   min-height: 100vh;
 `;
 
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-height: 100%;
+`;
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`;
+
 function App() {
   return (
-  <FundoGradiente>
-    <EstilosGlobais />
-    <Cabecalho />
-    <BarraLateral />
-  </FundoGradiente>
-  )
+    <FundoGradiente>
+      <EstilosGlobais />
+      <AppContainer>
+        <Cabecalho />
+        <MainContainer>
+          <BarraLateral />
+          <Banner
+            texto="A galeria mais completa de fotos do espaço!"
+            backgroundImage={bannerBackgroud}
+          />
+        </MainContainer>
+      </AppContainer>
+    </FundoGradiente>
+  );
 }
 
 export default App;
--- src/componetes/BarraLateral/index.jsx
+++ src/componetes/BarraLateral/index.jsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import ItemNavegacao from "./ItemNavegacao";
 
 const ListaEstilizada = styled.ul`
     list-style: none;
@@ -13,9 +14,32 @@ const BarraLateral = () => {
         <aside>
             <nav >
                 <ListaEstilizada>
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
+                   <ItemNavegacao 
+                    iconeAtivo="/icones/home-ativo.png" 
+                    iconeInativo="/icones/home-inativo.png"
+                    ativo= {true} >
+                            Inicio
+                   </ItemNavegacao>
+                   <ItemNavegacao 
+                    iconeAtivo="/icones/mais-vistas-ativo.png" 
+                    iconeInativo="/icones/mais-vistas-inativo.png">
+                            Mais vistas
+                   </ItemNavegacao>
+                   <ItemNavegacao 
+                    iconeAtivo="/icones/mais-vistas-ativo.png" 
+                    iconeInativo="/icones/mais-curtidas-inativo.png">
+                            Mais Curtidas
+                   </ItemNavegacao>
+                   <ItemNavegacao 
+                    iconeAtivo="/icones/mais-vistas-ativo.png" 
+                    iconeInativo="/icones/novas-inativo.png">
+                            Novas
+                   </ItemNavegacao>
+                   <ItemNavegacao 
+                    iconeAtivo="/icones/mais-vistas-ativo.png" 
+                    iconeInativo="/icones/surpreenda-me-inativo.png">
+                            Surpreenda-me
+                   </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>
